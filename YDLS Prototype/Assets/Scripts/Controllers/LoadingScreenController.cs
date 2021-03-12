a9f9584df0986e2b1943616d4a46807047b5854a
brydoescode <justawesomelikethat@gmail.com>
2021-03-11 20:48:52 -0500

    Added in additional loading screens for later use.

--- YDLS Prototype/Assets/Scripts/Controllers/LoadingScreenController.cs
+++ YDLS Prototype/Assets/Scripts/Controllers/LoadingScreenController.cs
@@ -3,25 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadingScreenController : MonoBehaviour
-{   
+{
+    [Header("Containers")]
     public GameObject loadingScreenContainer;
+
+    [Header("Controllers")]
     public GameController GameController;
+
     [Header("Loading Animations")]
+    public GameObject ambulanceLoading;
     public GameObject busLoading;
+    public GameObject carLoading;
     public GameObject sleepLoading;
+    public GameObject taxiLoading;
+    public GameObject trainLoading;
+    public GameObject vanLoading;
+    public GameObject walkingLoading;
 
     public void ChangeLoadingImage(string animationName)
     {
+        ambulanceLoading.SetActive(false);
         busLoading.SetActive(false);
+        carLoading.SetActive(false);
         sleepLoading.SetActive(false);
+        taxiLoading.SetActive(false);
+        trainLoading.SetActive(false);
+        vanLoading.SetActive(false);
+        walkingLoading.SetActive(false); 
 
         switch (animationName)
         {
+            case "ambulance":
+                ambulanceLoading.SetActive(true);
+                break;
             case "bus":
                 busLoading.SetActive(true);
                 break;
+            case "car":
+                carLoading.SetActive(true);
+                break;
             case "sleep":
                 sleepLoading.SetActive(true);
+                break;
+            case "taxi":
+                taxiLoading.SetActive(true);
+                break;
+            case "train":
+                trainLoading.SetActive(true);
+                break;
+            case "van":
+                vanLoading.SetActive(true);
+                break;
+            case "walking":
+                walkingLoading.SetActive(true);
                 break;
             default:
                 Debug.Log("Unknown Loading Animation: " + animationName);
@@ -39,7 +73,7 @@ public class LoadingScreenController : MonoBehaviour
     {
         loadingScreenContainer.SetActive(true);
         yield return new WaitForSeconds(2);
-        loadingScreenContainer.SetActive(false);
         GameController.OnClickContinueButton();
+        loadingScreenContainer.SetActive(false);
     }
 }
