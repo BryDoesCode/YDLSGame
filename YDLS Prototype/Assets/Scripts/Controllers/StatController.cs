d35dca584f9fd30fc0321af79dbd97efdc280db3
brydoescode <justawesomelikethat@gmail.com>
2021-04-27 21:34:11 -0400

    Additional UI edits.

--- YDLS Prototype/Assets/Scripts/Controllers/StatController.cs
+++ YDLS Prototype/Assets/Scripts/Controllers/StatController.cs
@@ -18,26 +18,40 @@ public class StatController : MonoBehaviour
     //public TextMeshProUGUI wellnessTopLabel;
     public ProgressBar topWellnessSlider;
 
+    private int energyStat;
+    private int healthStat;
+    private int wellnessStat;
+
+    [Header("Modal Containers")]
+    public GameObject mainModalContainer;
+    public GameObject energyModalContainer;
+    public TextMeshProUGUI energyModalStat;
+
+    public GameObject healthModalContainer;
+    public TextMeshProUGUI healthModalStat;
+
+    public GameObject wellnessModalContainer;
+    public TextMeshProUGUI wellnessModalStat;
 
     public void UpdateEnergyStat(int energy)
-    {
+    {        
         energySlider.UpdateCurrentFill(energy);
-        //energyTopLabel.text = energy.ToString();
         topEnergySlider.UpdateCurrentFill(energy);
+        energyStat = energy;
     }
 
     public void UpdateHealthStat(int health)
     {
         healthSlider.UpdateCurrentFill(health);
-        //healthTopLabel.text = health.ToString();
         topHealthSlider.UpdateCurrentFill(health);
+        healthStat = health;
     }
 
     public void UpdateWellnessStat(int wellness)
     {
         wellnessSlider.UpdateCurrentFill(wellness);
-        //wellnessTopLabel.text = wellness.ToString();
         topWellnessSlider.UpdateCurrentFill(wellness);
+        wellnessStat = wellness;
     }
 
     public void UpdateNeedMax(int maximum)
@@ -50,4 +64,35 @@ public class StatController : MonoBehaviour
         topWellnessSlider.SetMaximum(maximum);
     }
 
+    public void OnClickCloseModal()
+    {
+        mainModalContainer.SetActive(false);
+        energyModalContainer.SetActive(false);
+        healthModalContainer.SetActive(false);
+        wellnessModalContainer.SetActive(false);
+    }
+
+    public void OnClickEnergyModal()
+    {
+        mainModalContainer.SetActive(true);
+        energyModalContainer.SetActive(true);
+
+        energyModalStat.text = "Your current <color=#89a15c>Energy</color> is " + energyStat + ".";
+    }
+
+    public void OnClickHealthModal()
+    {
+        mainModalContainer.SetActive(true);
+        healthModalContainer.SetActive(true);
+
+        healthModalStat.text = "Your current <color=#9f4d3a>Health</color> is " + healthStat + ".";
+    }
+
+    public void OnClickWellnessModal()
+    {
+        mainModalContainer.SetActive(true);
+        wellnessModalContainer.SetActive(true);
+
+        wellnessModalStat.text = "Your current <color=#7a8f8b>Wellness</color> is " + wellnessStat + ".";
+    }
 }
